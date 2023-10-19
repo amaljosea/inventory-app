ff0620aee2bd303fc0566bd26c668ed29f8db65b
amal <howareyouami@gmail.com>
2023-10-19 12:00:11 +0100

    Add pages

--- context/InventoryContext.tsx
+++ context/InventoryContext.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { Product, StockItem } from "..";
 import { getInventoryData, updateInventoryData } from "@/data/inventoryData";
