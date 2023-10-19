ac7e07d7454eb7bb6b46aaa3b29d2a7f1e4468ce
amal <howareyouami@gmail.com>
2023-10-19 14:59:44 +0100

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 
 const Main = () => {
-  return <Layout title="Main " />;
+  return <Layout title="Main ">Welcome to the inventory app</Layout>;
 };
 
 export default Main;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,17 +6,15 @@ const Layout = ({
   children,
 }: {
   title: string;
-  children?: ReactNode;
+  children: ReactNode;
 }) => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="mt-10 text-red-700 font-bold">{title}</h1>
+      <h1 className="mt-10 text-red-700 font-bold ">{title}</h1>
       <Header />
-      {children && (
-        <div className="border-solid border-2 border-indigo-600 m-2 rounded p-2">
-          {children}
-        </div>
-      )}
+      <div className="border-solid border-2 border-grey-600 m-2 rounded p-2">
+        {children}
+      </div>
     </div>
   );
 };
