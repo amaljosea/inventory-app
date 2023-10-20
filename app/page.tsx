48958426ec30ad500341c7737995ca8c70241795
amal <howareyouami@gmail.com>
2023-10-20 18:19:23 +0200

    Dashboard update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
 import Layout from "@/components/Layout";
+import { useInventory } from "@/context/InventoryContext";
 
 const Main = () => {
-  return <Layout title="Main ">Welcome to the inventory app</Layout>;
+  const { data } = useInventory();
+
+  return (
+    <Layout title="Main ">
+      <div className="flex items-center justify-center flex-col">
+        <p>Welcome to the inventory app</p>
+
+        <div className="m-5">
+          <p>Types of products: {data.products.length}</p>
+          <p>
+            Total no of stock:{" "}
+            {data.products.reduce((acc, i) => acc + i.qty, 0)}
+          </p>
+          <p>Total no Stock Ins: {data.stockIns.length}</p>
+          <p>Total no Stock Outs: {data.stockOuts.length}</p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Main;
